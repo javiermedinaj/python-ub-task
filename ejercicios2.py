215c9a0c435d6baf71ee00342d3f3e75bbb464c2
medinajavierj <javier_j_medina@hotmail.com>
2024-05-13 13:05:48 -0300

    hoja2

--- ejercicios2.py
+++ ejercicios2.py
@@ -20,10 +20,36 @@
 
 # Escribir un programa que muestre, de a diez números por línea y separados por un blanco,
 # todos los números entre 100 y 1000 que sean divisibles por 5 y 6
-for i in range(100, 1001):
-    if i % 5 == 0 and i % 6 == 0:
-        print(i)
-        if i % 10 == 0:
-            print()
+# count = 0
+# for num in range(100, 1001):
+#     if num % 5 == 0 and num % 6 == 0:
+#         print(num, end=" ")
+#         count += 1
+#         if count % 10 == 0:
+#             print()
 
+
+#falta ejercicio 4 y 5
+
+
+# #ejercicio 6
+# Diseñar un programa que genere una lista de números usando el siguiente proceso: 
+# comenzar con un entero n que deberá ingresar el usuario. Si n es par, dividirlo por 2. Si n 
+# es impar, multiplicarlo por 3 y sumarle 1. Repetir este proceso hasta que el nuevo valor 
+# obtenido para n sea 1.
+# Ejemplo, para n = 22, la secuencia que se obtiene es: 
+# 22 11 33 17 52 26 13 40 20 10 5 16 8 4 2 1
+# def generar_lista(n):
+#     lista = [n]
+#     while n != 1:
+#         if n % 2 == 0:
+#             n //= 2
+#         else:
+#             n = 3 * n + 1
+#         lista.append(n)
+#     return lista
+
+# n = int(input("Ingrese un número entero positivo: "))
+# lista_generada = generar_lista(n)
+# print("Lista generada:", lista_generada)
 
